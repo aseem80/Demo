94dbb05157c3f28c1ed8a2e639bc9801b26fef84
aseem80 <aseem@tgtiio.com>
2017-04-05 22:56:07 -0700

    Updated .gitignore

---
+++ src/main/java/io/capitalone/interview/CodingAssignmentApp.java
@@ -0,0 +1,18 @@
+package io.capitalone.interview;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@SpringBootApplication
+public class CodingAssignmentApp
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(CodingAssignmentApp.class, args);
+    }
+}
